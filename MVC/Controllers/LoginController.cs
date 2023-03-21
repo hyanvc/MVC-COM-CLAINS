1e59301c5646df48c5e1377ebbd1be8e0a462a14
pfln99151 <hyan.gomes@capef.com.br>
2023-03-21 14:41:33 -0300

    Novas modificacoes de envio de email ( estrutura e  inicio da estrutura  EMAIL )

--- MVC/Controllers/LoginController.cs
+++ MVC/Controllers/LoginController.cs
@@ -18,6 +18,17 @@ using Microsoft.AspNetCore.Hosting.Server;
 using Grpc.Core;
 using System.IO;
 using OpenRasta.Configuration.Fluent;
+using OpenRasta.Collections;
+using OpenRasta.TypeSystem;
+using System.Diagnostics.Metrics;
+using System.Net;
+using RestSharp;
+using Newtonsoft.Json;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+using System.Net.Mail;
+using Twilio.TwiML.Messaging;
 
 namespace MVC.Controllers
 {
@@ -90,7 +101,7 @@ namespace MVC.Controllers
         }
         public string generarReportePDF(string nombre)
         {
-
+            
             string filename = "hyan.pdf";
             var physicalPath = $"./{filename}";
             var pdfBytes = System.IO.File.ReadAllBytes(physicalPath);
@@ -99,10 +110,76 @@ namespace MVC.Controllers
         }
 
 
+        //ZENVIA ENVIO DE SMS//
+        public  JsonResult Send(SMSBuilder builder)
+        {
+            var blz = "ok";
+            SendEmail(blz);
+            builder.To = "5585988190112";
+            builder.From = "5585988190112";
+            builder.text = "teste!";
+            var tel = "text";
 
+            List<SMSBuilder> contents = new List<SMSBuilder>
+            {
+                new SMSBuilder {text = builder.text, type = tel}
+
+            };
+            var tall = new { from = builder.From, to = builder.To, contents };
+            var body = JsonConvert.SerializeObject(tall);
+            var client = new RestClient("https://api.zenvia.com/v2/channels/sms/messages");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("X-API-TOKEN", "hL7Jxb8-4ma9GqZzvwRnvCxx2NHMUADoxeZq");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; //verificar pois tem que ser tls12.
+            var response = client.Execute(request);
+            var resultados = response.StatusCode;
+
+            return Json(new { re = resultados });
+
+
+        }
+        //TWILLO SMS//https://console.twilio.com/us1/develop/sms/try-it-out/send-an-sms
+        //HyanVictorCunhaGomes
+        static MessageResource SendSMS(string message)
+        {
+            message = "OI";
+            var accountSid = "AC75a01835c9897135ed1f2d77b954cc15";
+            var authToken = "6f5310d40d6992ffe79c17ca2506af60";
+            TwilioClient.Init(accountSid, authToken);
+
+            var messageOptions = new CreateMessageOptions(
+                new PhoneNumber("+5585988190112"));
+            messageOptions.MessagingServiceSid = "MGccd362e20e68bf8b17d6d6e10a53be39";
+            messageOptions.From = new PhoneNumber("+15177900641");
+            messageOptions.Body = message;
+
+           var T = MessageResource.Create(messageOptions);
+            var k = T.Status;
+            return T;
+        }
 
         public JsonResult obteridade(string txtData)
         {
+
+            SMSBuilder builder = new SMSBuilder();
+            SendSMS(txtData);
+
+            var l = new List<string>();
+            var y = txtData.Split();
+            string[] strg;
+            foreach (var i in y)
+            {
+                if (i == "b")
+                {
+                    l.Add(i);
+                }
+            }
+
+            var splitado = l;
+
+            var t = txtData.Substring(0, 100);
             DateTime dataNascimento = Convert.ToDateTime(txtData);
 
             int anos = DateTime.Today.Year - dataNascimento.Year;
@@ -112,6 +189,43 @@ namespace MVC.Controllers
 
 
             return Json(new { anos = anos });
+        }
+
+
+        //ESTRUTURA DE ENVIAR EMAIL EM DESENVOLVIMENTO //
+        public static void SendEmail(string e2mail)
+        { // Configurar as credenciais do remetente
+            //var fromAddress = new MailAddress("hyancunha@gmail.com", "Hyan");
+            //var fromPassword = "hyan1234";
+
+            //// Configurar as informações do destinatário
+            //var toAddress = new MailAddress("hyann1@hotmail.com", "Destinatário");
+
+            //// Configurar o corpo do e-mail
+            //const string subject = "Assunto ";
+            //const string body = "Conteúdo";
+
+            //// Configurar o cliente SMTP do Gmail
+            //var smtp = new SmtpClient
+            //{
+            //    Host = "smtp.gmail.com",
+            //    Port = 587,
+            //    EnableSsl = true,
+            //    DeliveryMethod = SmtpDeliveryMethod.Network,
+            //    UseDefaultCredentials = false,
+            //    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            //};
+
+            //// Enviar o e-mail
+            //using (var message = new MailMessage(fromAddress, toAddress)
+            //{
+            //    Subject = subject,
+            //    Body = body
+            //})
+            //{
+            //    smtp.Send(message);
+            //}
+
         }
 
 
---
+++ MVC/Controllers/SMSBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Controllers
+{
+    public class SMSBuilder
+    {
+        public string type { get; set; }
+        public string text;
+        public string From { get; internal set; }
+        public string To { get; internal set; }
+    }
+}
