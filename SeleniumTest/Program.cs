2b84a2163672a4967b4a64e8e184a06931f6aca6
hyann1 <hyann1@hotmail.com>
2023-04-12 22:01:23 -0300

    adicionado logica do gpt terceirizado

--- SeleniumTest/Program.cs
+++ SeleniumTest/Program.cs
@@ -23,17 +23,39 @@ class Program
             IWebElement messageBox = driver.FindElement(By.XPath("//div[@contenteditable='true'][@data-tab='10']"));
             messageBox.SendKeys(" vou te enviar mensagens a cada 10 secs." + Keys.Enter);
             Thread.Sleep(10000);
-
         }
 
 
+        // LOGICA PRA UMA ALTERNATIVA DE CHAT GPT
 
-        //// Obter identificadores de janela
-        //var windowHandles = driver.WindowHandles;
 
-        //// Trocar para a segunda aba
-        //driver.SwitchTo().Window(windowHandles[1]);
 
-        driver.Quit();
+        //// Adiciona uma nova aba
+        //driver.FindElement(By.CssSelector("body")).SendKeys(Keys.Control + "t");
+
+        //// Navega para o Google na nova aba
+        //driver.SwitchTo().Window(driver.WindowHandles.Last());
+        //driver.Navigate().GoToUrl("https://app.writesonic.com/"); 
+        //IWebElement btnEnviar =  driver.FindElement(By.XPath("//p[@class='text-sm text-gray-6 group-hover:text-navy-1 whitespace-normal min-h-17']"));
+        //btnEnviar.Click();
+
+        ////enviar mensagem no chat gpt alternativo
+        //IWebElement messageBox = driver.FindElement(By.CssSelector("textarea"));
+        //messageBox.SendKeys("orkut" + Keys.Enter);
+
+
+        ////PEGAR O TEXTO
+        //IWebElement chatsonicDiv = driver.FindElement(By.CssSelector("div.chatsonic"));
+        //string chatsonicText = chatsonicDiv.Text;
+        //// APAGAR HISTORICO//
+        //IWebElement btnEnviar2 = driver.FindElement(By.XPath("//button[contains(., 'New Chat')]"));
+
+        //btnEnviar2.Click();
+
+
+
+
+
+        //driver.Quit();
     }
 }
