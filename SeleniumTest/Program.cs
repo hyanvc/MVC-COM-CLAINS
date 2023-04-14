aeb9473454a123d2c27725180b8b673af811421b
hyann1 <hyann1@hotmail.com>
2023-04-14 08:46:22 -0300

    logica de pegar ultima mensagem implementada.

--- SeleniumTest/Program.cs
+++ SeleniumTest/Program.cs
@@ -6,37 +6,39 @@ class Program
     static void Main(string[] args)
     {
 
-        IWebDriver driver = new ChromeDriver();
-        driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/login?with_password=true");
-        IWebElement login = driver.FindElement(By.Id("email"));
-        login.SendKeys("hyann1@hotmail.com");
+        // LOGICA TERCEIRIZADA GPT //  CHATSONIC
 
-        IWebElement senhalogin = driver.FindElement(By.CssSelector("input[type='password']"));
-        senhalogin.SendKeys("hyan1020");
-        senhalogin.SendKeys(Keys.Enter);
+        //IWebDriver driver = new ChromeDriver();
+        //driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/login?with_password=true");
+        //IWebElement login = driver.FindElement(By.Id("email"));
+        //login.SendKeys("hyann1@hotmail.com");
 
-        driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/06a38aa1-0dca-47ad-a506-be0fc31051ab/chatsonic/82b308a6-4e91-4f07-9045-6949b098ae6b");
-        IWebElement textarea = driver.FindElement(By.CssSelector("textarea[data-gramm_editor='false']"));
-        textarea.SendKeys("Musica do zeze de camargo é o amor" + Keys.Enter);
-        IWebElement chatsonicElement = driver.FindElement(By.CssSelector(".chatsonic p"));
-        string chatsonicText = chatsonicElement.Text;
-        driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/06a38aa1-0dca-47ad-a506-be0fc31051ab/chatsonic/82b308a6-4e91-4f07-9045-6949b098ae1a");
+        //IWebElement senhalogin = driver.FindElement(By.CssSelector("input[type='password']"));
+        //senhalogin.SendKeys("hyan1020");
+        //senhalogin.SendKeys(Keys.Enter);
 
-        //Random random = new Random();
-        //string letras = "abcdefghijklmnopqrstuvwxyz";
-        //string numero = "0123456789";
+        //driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/06a38aa1-0dca-47ad-a506-be0fc31051ab/chatsonic/82b308a6-4e91-4f07-9045-6949b098ae6b");
+        //IWebElement textarea = driver.FindElement(By.CssSelector("textarea[data-gramm_editor='false']"));
+        //textarea.SendKeys("Musica do zeze de camargo é o amor" + Keys.Enter);
+        //IWebElement chatsonicElement = driver.FindElement(By.CssSelector(".chatsonic p"));
+        //string chatsonicText = chatsonicElement.Text;
+        //driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/06a38aa1-0dca-47ad-a506-be0fc31051ab/chatsonic/82b308a6-4e91-4f07-9045-6949b098ae1a");
 
-        //string randomString = letras[random.Next(letras.Length)].ToString() +
-        //                      letras[random.Next(letras.Length)].ToString() +
-        //                      numero[random.Next(numero.Length)].ToString();
+        ////Random random = new Random();
+        ////string letras = "abcdefghijklmnopqrstuvwxyz";
+        ////string numero = "0123456789";
+
+        ////string randomString = letras[random.Next(letras.Length)].ToString() +
+        ////                      letras[random.Next(letras.Length)].ToString() +
+        ////                      numero[random.Next(numero.Length)].ToString();
 
 
-        // LOGICA PRA REFAZER A PESQUISA NO CHAT //
+        //// LOGICA PRA REFAZER A PESQUISA NO CHAT //
 
-        char currentLetter = 'a';
-        currentLetter++;
+        //char currentLetter = 'a';
+        //currentLetter++;
 
-        driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/9519563e-e02b-4232-b53b-b0b0df2ae171/chatsonic/d4f6474b-6c53-43bb-9838-c79f8545801" + currentLetter );
+        //driver.Navigate().GoToUrl("https://app.writesonic.com/pt-pt/template/9519563e-e02b-4232-b53b-b0b0df2ae171/chatsonic/d4f6474b-6c53-43bb-9838-c79f8545801" + currentLetter );
 
 
 
@@ -97,59 +99,60 @@ class Program
 
 
 
-
-
-
-
-
-
-
-
         //WHATS APP//
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://web.whatsapp.com/");
+        Thread.Sleep(15000);
+        // Encontre a caixa de pesquisa e pesquise pelo usuário "nabaaau"
+        IWebElement searchBox = driver.FindElement(By.XPath("//div[@contenteditable='true']"));
+        searchBox.SendKeys("imbecil" + Keys.Enter);
+        Thread.Sleep(10000);
+
+        // Aguarde o chat do usuário carregar
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        Thread.Sleep(10000);
+        while (true)
+        {
+            IWebElement messageBox = driver.FindElement(By.XPath("//div[@contenteditable='true'][@data-tab='10']"));
+            messageBox.SendKeys(" vou te enviar mensagens a cada 10 secs, ok IMBE?" + Keys.Enter);
+            //Thread.Sleep(1000);
+        }
+
+
+
+
+
+
+        //WHATS APP COM LOGICA DE ULTIMA MENSAGEM//
         //IWebDriver driver = new ChromeDriver();
         //driver.Navigate().GoToUrl("https://web.whatsapp.com/");
         //Thread.Sleep(15000);
         //// Encontre a caixa de pesquisa e pesquise pelo usuário "nabaaau"
         //IWebElement searchBox = driver.FindElement(By.XPath("//div[@contenteditable='true']"));
-        //searchBox.SendKeys("imbecil" + Keys.Enter);
+        //searchBox.SendKeys("Suryah Almeida" + Keys.Enter);
         //Thread.Sleep(10000);
 
         //// Aguarde o chat do usuário carregar
         //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         //Thread.Sleep(10000);
-        //while (true)
-        //{
-        //    IWebElement messageBox = driver.FindElement(By.XPath("//div[@contenteditable='true'][@data-tab='10']"));
-        //    messageBox.SendKeys(" vou te enviar mensagens a cada 10 secs, ok IMBE?" + Keys.Enter);
-        //    //Thread.Sleep(1000);
-        //}
-
-
-        // LOGICA PRA UMA ALTERNATIVA DE CHAT GPT
+        //IWebElement messageContainer = driver.FindElement(By.CssSelector("div[data-tab='8']"));
 
 
 
-        //// Adiciona uma nova aba
-        //driver.FindElement(By.CssSelector("body")).SendKeys(Keys.Control + "t");
-
-        //// Navega para o Google na nova aba
-        //driver.SwitchTo().Window(driver.WindowHandles.Last());
-        //driver.Navigate().GoToUrl("https://app.writesonic.com/"); 
-        //IWebElement btnEnviar =  driver.FindElement(By.XPath("//p[@class='text-sm text-gray-6 group-hover:text-navy-1 whitespace-normal min-h-17']"));
-        //btnEnviar.Click();
-
-        ////enviar mensagem no chat gpt alternativo
-        //IWebElement messageBox = driver.FindElement(By.CssSelector("textarea"));
-        //messageBox.SendKeys("orkut" + Keys.Enter);
+        ////ENCONTRA A ULTIMA MENSAGEM ENVIADA NO WPP //
+        //IWebElement lastMessage = messageContainer.FindElement(By.XPath(".//div[@class='_2WxO4']/div[last()]//span[contains(@class, 'selectable-text')][last()]"));
+        //List<IWebElement> listademensagens = messageContainer.FindElements(By.ClassName("focusable-list-item")).ToList();
+        //IWebElement ultimamensagem = listademensagens.LastOrDefault();
+        //string ultimamensagemdetexto = ultimamensagem.FindElement(By.ClassName("_11JPr")).Text;
 
 
-        ////PEGAR O TEXTO
-        //IWebElement chatsonicDiv = driver.FindElement(By.CssSelector("div.chatsonic"));
-        //string chatsonicText = chatsonicDiv.Text;
-        //// APAGAR HISTORICO//
-        //IWebElement btnEnviar2 = driver.FindElement(By.XPath("//button[contains(., 'New Chat')]"));
+        ////while (true)
+        ////{
+        //IWebElement messageBox = driver.FindElement(By.XPath("//div[@contenteditable='true'][@data-tab='10']"));
+        //messageBox.SendKeys(ultimamensagemdetexto + Keys.Enter);
+        //Thread.Sleep(1000);
+        ////}
 
-        //btnEnviar2.Click();
 
 
 
